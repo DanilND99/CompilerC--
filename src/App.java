a0d34b024ccbd4080521b813d6197decb5120ce3
DanilN1 <daniel.armando.nd@gmail.com>
2021-04-01 15:30:55 -0700

    Character Identification
    The identification of every character in the language by the lexical analyzer

--- src/App.java
+++ src/App.java
@@ -5,6 +5,10 @@ public class App {
     public static ArrayList<Symbol> symbolTable = new ArrayList<Symbol>();
     public static TokenTable tokenTable = new TokenTable();
     public static void main(String[] args) throws Exception {
+        Lexical lexAnalyzer = new Lexical();
+        lexAnalyzer.iterate("if(x <= 4){\nx++;\n}else{\nx--;\n} ABC@");
+        /*
+        lexAnalyzer.test(tokenTable, symbolTable);
         symbolTable.add(new Symbol(1,"keyword"));
         symbolTable.add(new Symbol(2,"keyword"));
         symbolTable.add(new Symbol(15,"special"));
@@ -16,8 +20,10 @@ public class App {
         System.out.println(tokenTable.find("variabe")+ "\n");
         System.out.println(tokenTable.getKey("variable")+ "\n");
         System.out.println(tokenTable.getKey("variabe")+ "\n");
+        lexAnalyzer.test(tokenTable, symbolTable);
         tokenTable.printTable();
         printSymbolTable();
+        */
     }
 
     //Print the symbol table.
--- src/com/analyzers/Lexical.java
+++ src/com/analyzers/Lexical.java
@@ -1,5 +1,49 @@
 package com.analyzers;
-
+import java.util.ArrayList;
+import com.utils.*;
 public class Lexical {
-    
+    public void iterate(String x){
+        char character;
+        System.out.println(x);
+        for(int i = 0;i < x.length();i++){
+            character = x.charAt(i);
+            if(character >= 48 && character <= 57){
+                System.out.println("number");
+            }else if(character == 32 || character == 9 || character == 10){
+                System.out.println("whitespace");
+            }else if((character >= 65 && character <= 90) || (character >= 97 && character <= 122)){
+                System.out.println("letter");
+            }else{
+                switch(character){
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                    case '=':
+                    case '<':
+                    case '>':
+                    case '(':
+                    case ')':
+                    case '[':
+                    case ']':
+                    case '{':
+                    case '}':
+                    case '!':
+                    case ',':
+                    case ';':
+                        System.out.println("Special character");
+                        break;
+                    default:
+                        System.out.println(character);
+                }
+                //System.out.println(character);
+            }
+        }
+        System.out.println("\tend\n");
+    }
+
+    public void test(TokenTable tokTable, ArrayList<Symbol> symTable){
+        tokTable.set("test");
+        symTable.add(new Symbol(90,"Test"));
+    }
 }
